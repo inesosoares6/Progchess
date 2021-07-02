f23524f890a9d0ce9cfb753bb5f1408a97fda8f4
Inês Soares <inesosoares.6@gmail.com>
2021-07-02 17:49:45 +0100

    Board Setup - level 3 complete

---
+++ Assets/BoardSetupManager.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BoardSetupManager : MonoBehaviour
+{
+    public BoardSetupLevel3 boardSetupLevel3;
+
+    public void initiateLevel3()
+    {
+        boardSetupLevel3.showPieces();
+    }
+}
