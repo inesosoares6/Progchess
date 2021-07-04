a504e80743d7820efa6ae576f1538a0c04924996
Inês Soares <inesosoares.6@gmail.com>
2021-07-04 16:49:11 +0100

    Board Setup - level 1 and 2 complete

---
+++ Assets/Scripts/BoardSetupManager.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BoardSetupManager : MonoBehaviour
+{
+    public BoardSetupLevel1 boardSetupLevel1;
+    public BoardSetupLevel2 boardSetupLevel2;
+    public BoardSetupLevel3 boardSetupLevel3;
+    public BoardSetupLevel4 boardSetupLevel4;
+    public GameObject manager;
+
+    public void initiateLevel1()
+    {
+        manager.GetComponent<BoardSetupLevel1>().enabled = true;
+        boardSetupLevel1.initiatelevel1();
+    }
+
+    public void initiateLevel2()
+    {
+        boardSetupLevel2.initiateLevel2();
+    }
+
+    public void initiateLevel3()
+    {
+        boardSetupLevel3.showPieces();
+    }
+
+    public void initiateLevel4()
+    {
+        boardSetupLevel4.showPieces();
+    }
+}
---
+++ Assets/Scripts/TestMovePiece.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestMovePiece : MonoBehaviour
+{
+    public GameObject square;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        square.transform.position = new Vector3(-4.375f, 0.01f, -4.375f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void moveRight()
+    {
+        square.transform.position = square.transform.position + new Vector3(1.25f, 0.0f, 0.0f);
+    }
+}
