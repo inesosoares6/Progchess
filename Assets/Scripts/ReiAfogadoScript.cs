661d6740d87b6a365784144f7d411cb98c4ef838
Inês Soares <inesosoares.6@gmail.com>
2021-09-11 18:04:53 +0100

    Attacks on the king - saida xeque setup

---
+++ Assets/Scripts/ReiAfogadoScript.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class ReiAfogadoScript : MonoBehaviour
+{
+    private int level;
+    public GameObject buttonLevels;
+    public GameObject back2pieces;
+    public GameObject back2scenes;
+    public GameObject buttonPieces;
+    public AudioSource levelUp;
+    public void defineLevel_reiafogado(int num)
+    {
+        level = num;
+        beginReiAfogado();
+        moveTarget_reiafogado(level);
+    }
+
+    private void moveTarget_reiafogado(int level)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void showLevels_reiafogado()
+    {
+        buttonLevels.SetActive(true);
+        buttonPieces.SetActive(false);
+        back2scenes.SetActive(false);
+        back2pieces.SetActive(true);
+        //if (squares_introduced)
+        //{
+        //    DeleteSquares_tower();
+        //    targets["target1"].SetActive(false);
+        //    targets["target2"].SetActive(false);
+        //}
+        //tower2.SetActive(false);
+        //tower.transform.position = new Vector3(-4.375f, 0.0f, 4.375f);
+    }
+
+    public void beginReiAfogado()
+    {
+        buttonLevels.SetActive(false);
+        back2pieces.SetActive(false);
+    }
+}
